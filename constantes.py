24ac34c5555d93208c32b76285840a555a74f962
Helena <helenaac@al.insper.edu.br>
2024-04-24 17:45:31 -0300

    constantes

--- constantes.py
+++ constantes.py
@@ -49,6 +49,8 @@ PAISES =  {
 
 # alfabeto para montar o nome das colunas
 ALFABETO = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# numeros para nome das linhas 
+linhas = [' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26']
 
 # cores para o terminal
 CORES = {
