abbe20e0c5ac72424f9dd63b0df249dafe6e8233
catharinach <catharinach@al.insper.edu.br>
2024-04-24 13:43:50 -0300

    primeira parte da tela inicial

--- tela.py
+++ tela.py
@@ -1,1 +1,19 @@
-ola = 3
+import random
+from constantes import *
+
+print(' ')
+print('Bem-vindo ao INSPER - Batalha Naval')
+print(' ')
+print('Iniciando o jogo!')
+print(' ')
+
+lista_paises = []
+for pais in PAISES.keys():
+    lista_paises.append(pais)
+
+pais_escolhido = random.choice(lista_paises)
+
+print(f'Computador está alocando os navios de guerra do país {pais_escolhido}.')
+print('Computador já está em posição de batalha!')
+print(' ')
+
