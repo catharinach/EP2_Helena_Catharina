98d23a2183e736e71fe764f7cc018c1b3321523d
catharinach <catharinach@al.insper.edu.br>
2024-04-24 09:13:51 -0300

    funcao

--- operacoes.py
+++ operacoes.py
@@ -1,1 +1,11 @@
 from constantes import * 
+
+def cria_mapa(n):
+    lista = [' ']*n
+    lista2 = [lista]*n
+    return lista2
+
+def cria_mapa(n):
+    lista = [' ']*n
+    lista2 = [lista]*n
+    return lista2
