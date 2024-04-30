0c069f75fbdab1ea482a56977a84753c0197a7dc
catharinach <catharinach@al.insper.edu.br>
2024-04-30 15:03:56 -0300

    tentativa de organizar o codigo

--- operacoes.py
+++ operacoes.py
@@ -10,9 +10,8 @@ def cria_mapa(n):
 
 def posicao_suporta(mapa, blocos, linha, coluna, orientacao): 
     tamanho = len(mapa)
-    
-    if linha < 1 or coluna < 1 or linha >= tamanho or coluna >= tamanho:
-        return False
+    if mapa[linha][coluna] != ' ': 
+        return False 
     
     if orientacao == 'v': 
         if linha + blocos > tamanho:
@@ -27,6 +26,23 @@ def posicao_suporta(mapa, blocos, linha, coluna, orientacao):
             if mapa[linha][j] != ' ': 
                 return False 
             
+    return True 
+            
+def aloca_navios2(mapa, lista):
+    n = 10
+    linha = random.randint(0, n-1)
+    coluna = random.randint(0, n-1)
+    orientacao = random.choice(['h', 'v'])
+    for bloco in lista:
+        x = posicao_suporta(mapa, bloco, linha, coluna, orientacao)
+        while x == False:
+            linha = random.randint(0, n-1)
+            coluna = random.randint(0, n-1)
+            orientacao = random.choice(['h', 'v'])
+            x = posicao_suporta(mapa, bloco, linha, coluna, orientacao)
+        mapa_computador = alocando2(mapa, bloco, linha, coluna, orientacao)
+
+    return mapa_computador
 
 def alocando(mapa, b, l, c, o):
     if o == 'v':
@@ -65,14 +81,14 @@ def aloca_navios(mapa, lista):
         mapa = alocando(mapa, bloco, linha, coluna, orientacao)
     return mapa
 
+
+
 def foi_derrotado(matriz):
     x = True
     for lista in matriz:
         if 'N' in lista:
             x = False
     return x    
-
-
 
 # Imprimir navios a serem alocados 
 
@@ -116,33 +132,40 @@ def criar_mapa():
 
 # Correcao na impressao dos mapas 
 
-def criar_mapa():
+def criar_mapa2():
     matriz_adversario = []
     matriz_jogador = []
 
-    for i in range(12):
-        linha_adversario = []
-        linha_jogador = []
-
-        if i == 0 or i == 11:
-            linha_adversario = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
-            linha_jogador = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
-        else:
-            linha_adversario = [' '] * 12
-            linha_jogador = [' '] * 12
-
-        if 1 <= i <= 10:
+    for i in range(1, 11):
+        linha_adversario = [' '] * 11
+        linha_jogador = [' '] * 11
+        if i >= 1 and i <= 9:
             nmr = str(i)
-            linha_adversario[0] = nmr
-            linha_jogador[0] = nmr
-            linha_adversario[-1] = nmr
-            linha_jogador[-1] = nmr
-
+            lista = [' ', nmr]
+            linha_adversario[0] = str(i)
+            linha_jogador[0] = str(i)
+            linha_adversario[-1] = ''.join(lista)
+            linha_jogador[-1] = ''.join(lista)
+        elif i == 10:
+            linha_adversario[0] = str(i)
+            linha_jogador[0] = str(i)
+            linha_adversario[-1] = str(i)
+            linha_jogador[-1] = str(i)
         matriz_adversario.append(linha_adversario)
         matriz_jogador.append(linha_jogador)
+    
+    matriz_adversario.insert(0, [' '] + [letra for letra in ALFABETO[:10]] + [' '])
+    matriz_jogador.insert(0, [' '] + [letra for letra in ALFABETO[:10]] + [' '])
+    matriz_adversario.insert(11, [' '] + [letra for letra in ALFABETO[:10]] + [' '])
+    matriz_jogador.insert(11, [' '] + [letra for letra in ALFABETO[:10]] + [' '])
+    
+    print("Mapa do adversário:".center(20), "Seu mapa:".center(30))
+    print( )
+    for linha1, linha2 in zip(matriz_jogador, matriz_adversario):
+        print("{:<25} {:<25}".format(" ".join(linha1), " ".join(linha2))) 
 
-    return matriz_jogador, matriz_adversario
-
+    matrizes = [matriz_jogador, matriz_adversario]
+    return matrizes  
 
 def mapa_formatado(mapa_jogador, mapa_adversario):
     matriz_jogador = []
@@ -180,3 +203,85 @@ def print_maps(matriz_adversario, mapa_usuario):
         print(linha_adv_str, "   ", linha_jog_str)
 
 
+def mapa_formatado(mapa_jogador, mapa_adversario):
+    matriz_jogador = []
+    matriz_adversario = []
+
+    max_rows = max(len(mapa_jogador), len(mapa_adversario))
+
+    for i in range(max_rows):
+        linha_jogador = [' '] * 12
+        linha_adversario = [' '] * 12
+
+        if 0 < i < 11:
+            if i < len(mapa_jogador):
+                linha_jogador[:len(mapa_jogador[i])] = mapa_jogador[i]
+            if i < len(mapa_adversario):
+                linha_adversario[:len(mapa_adversario[i])] = mapa_adversario[i]
+
+        if i == 0 or i == 11:
+            linha_jogador = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
+            linha_adversario = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
+
+        matriz_jogador.append(linha_jogador)
+        matriz_adversario.append(linha_adversario)
+
+    return matriz_jogador, matriz_adversario
+
+def mapa_formatado(mapa_jogador, mapa_adversario):
+    matriz_jogador = []
+    matriz_adversario = []
+
+    for i in range(12):
+        linha_jogador = []
+        linha_adversario = []
+
+        if i == 0 or i == 11:
+            linha_jogador = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
+            linha_adversario = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
+        else:
+            linha_jogador = mapa_jogador[i]
+            linha_adversario = mapa_adversario[i]
+
+        matriz_jogador.append(linha_jogador)
+        matriz_adversario.append(linha_adversario)
+
+    return matriz_jogador, matriz_adversario 
+
+def criar_mapa():
+    matriz_adversario = []
+    matriz_jogador = []
+
+    for i in range(12):
+        linha_adversario = []
+        linha_jogador = []
+        if i == 0 or i == 11:
+            linha_adversario = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
+            linha_jogador = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
+        else:
+            linha_adversario = [' '] * 12
+            linha_jogador = [' '] * 12
+            if i >= 1 and i <= 9:
+                nmr = str(i)
+                lista = [' ', nmr]
+                linha_adversario[0] = str(i)
+                linha_jogador[0] = str(i)
+                linha_adversario[-1] = ''.join(lista)
+                linha_jogador[-1] = ''.join(lista)
+            elif i == 10:
+                linha_adversario[0] = str(i)
+                linha_jogador[0] = str(i)
+                linha_adversario[-1] = str(i)
+                linha_jogador[-1] = str(i)
+        matriz_adversario.append(linha_adversario)
+        matriz_jogador.append(linha_jogador)
+    
+
+
+    print("Mapa do adversário:".center(20), "Seu mapa:".center(40))
+    print( )
+    for linha_adv, linha_jog in zip(matriz_adversario, matriz_jogador):
+        print(" ".join(linha_adv).ljust(30), "", " ".join(linha_jog)) 
+
+    matrizes = [matriz_jogador, matriz_adversario]
+    return matrizes  
--- teste_nova_funcao.py
+++ teste_nova_funcao.py
@@ -8,42 +8,6 @@ print(' ')
 print('Iniciando o jogo!')
 print(' ')
 
-def aloca_navios2(mapa, lista):
-    n = 10
-    linha = random.randint(0, n-1)
-    coluna = random.randint(0, n-1)
-    orientacao = random.choice(['h', 'v'])
-    for bloco in lista:
-        x = posicao_suporta(mapa, bloco, linha, coluna, orientacao)
-        while x == False:
-            linha = random.randint(0, n-1)
-            coluna = random.randint(0, n-1)
-            orientacao = random.choice(['h', 'v'])
-            x = posicao_suporta(mapa, bloco, linha, coluna, orientacao)
-        mapa_computador = alocando2(mapa, bloco, linha, coluna, orientacao)
-
-    return mapa_computador
-
-
-def posicao_suporta(mapa, blocos, linha, coluna, orientacao): 
-    tamanho = len(mapa)
-    if mapa[linha][coluna] != ' ': 
-        return False 
-    
-    if orientacao == 'v': 
-        if linha + blocos > tamanho:
-            return False 
-        for i in range(linha, linha + blocos): 
-            if mapa[i][coluna] != ' ': 
-                return False 
-    elif orientacao == 'h': 
-        if coluna + blocos > tamanho: 
-            return False 
-        for j in range(coluna,  coluna + blocos): 
-            if mapa[linha][j] != ' ': 
-                return False 
-            
-    return True 
 
 #escolhendo pais aleatorio para o computador
 lista_paises = []
@@ -110,40 +74,6 @@ for pais, info in PAISES.items():
             for i in range(qtd):
                 lista.append(CONFIGURACAO[navio])
 
-def criar_mapa2():
-    matriz_adversario = []
-    matriz_jogador = []
-
-    for i in range(1, 11):
-        linha_adversario = [' '] * 11
-        linha_jogador = [' '] * 11
-        if i >= 1 and i <= 9:
-            nmr = str(i)
-            lista = [' ', nmr]
-            linha_adversario[0] = str(i)
-            linha_jogador[0] = str(i)
-            linha_adversario[-1] = ''.join(lista)
-            linha_jogador[-1] = ''.join(lista)
-        elif i == 10:
-            linha_adversario[0] = str(i)
-            linha_jogador[0] = str(i)
-            linha_adversario[-1] = str(i)
-            linha_jogador[-1] = str(i)
-        matriz_adversario.append(linha_adversario)
-        matriz_jogador.append(linha_jogador)
-    
-    matriz_adversario.insert(0, [' '] + [letra for letra in ALFABETO[:10]] + [' '])
-    matriz_jogador.insert(0, [' '] + [letra for letra in ALFABETO[:10]] + [' '])
-    matriz_adversario.insert(11, [' '] + [letra for letra in ALFABETO[:10]] + [' '])
-    matriz_jogador.insert(11, [' '] + [letra for letra in ALFABETO[:10]] + [' '])
-    
-    print("Mapa do adversário:".center(20), "Seu mapa:".center(30))
-    print( )
-    for linha1, linha2 in zip(matriz_jogador, matriz_adversario):
-        print("{:<25} {:<25}".format(" ".join(linha1), " ".join(linha2))) 
-
-    matrizes = [matriz_jogador, matriz_adversario]
-    return matrizes  
 
 mapa_u, mapa_adv = criar_mapa2()
 
@@ -183,21 +113,6 @@ while frota_escolhida:
            print(f'Navio a ser alocado: 1 {navio} (Tamanho: {tamanho_navio} blocos)')
            print() 
            z = aloca_navios3(mapa_u)
-           #fila = int(input(f"Digite o número da fila para o {navio}: "))
-           #coluna = ALFABETO.index(input(f"Digite a letra da coluna para o {navio} (A-J): ").upper()) + 1
-           #orientacao = input("Digite 'h' para horizontal ou 'v' para vertical: ") 
-
-           #x = posicao_suporta(mapa_u, tamanho_navio, fila, coluna, orientacao) 
-           
-           #while x == False:
-            #   print('Não é possível colocar um navio nessa posição... Tente novamente.')
-            #   fila = int(input(f"Digite o número da fila para o {navio}: "))
-            #   coluna = ALFABETO.index(input(f"Digite a letra da coluna para o {navio} (A-J): ").upper()) + 1
-            #   orientacao = input("Digite 'h' para horizontal ou 'v' para vertical: ") 
-            #   x = posicao_suporta(mapa_u, tamanho_navio, fila, coluna, orientacao)
-            #   print('')
-        #y = alocando(mapa_u, tamanho_navio, fila, coluna, orientacao)
-        
         print ('')
 
         print("Mapa do adversário:".center(20), "Seu mapa:".center(40))
@@ -210,91 +125,7 @@ while frota_escolhida:
             del frota_escolhida[navio]
         break 
 
-
-def mapa_formatado(mapa_jogador, mapa_adversario):
-    matriz_jogador = []
-    matriz_adversario = []
-
-    max_rows = max(len(mapa_jogador), len(mapa_adversario))
-
-    for i in range(max_rows):
-        linha_jogador = [' '] * 12
-        linha_adversario = [' '] * 12
-
-        if 0 < i < 11:
-            if i < len(mapa_jogador):
-                linha_jogador[:len(mapa_jogador[i])] = mapa_jogador[i]
-            if i < len(mapa_adversario):
-                linha_adversario[:len(mapa_adversario[i])] = mapa_adversario[i]
-
-        if i == 0 or i == 11:
-            linha_jogador = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
-            linha_adversario = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
-
-        matriz_jogador.append(linha_jogador)
-        matriz_adversario.append(linha_adversario)
-
-    return matriz_jogador, matriz_adversario
-
-def mapa_formatado(mapa_jogador, mapa_adversario):
-    matriz_jogador = []
-    matriz_adversario = []
-
-    for i in range(12):
-        linha_jogador = []
-        linha_adversario = []
-
-        if i == 0 or i == 11:
-            linha_jogador = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
-            linha_adversario = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
-        else:
-            linha_jogador = mapa_jogador[i]
-            linha_adversario = mapa_adversario[i]
-
-        matriz_jogador.append(linha_jogador)
-        matriz_adversario.append(linha_adversario)
-
-    return matriz_jogador, matriz_adversario 
-
-#print("Mapa do adversário:".center(20), "Seu mapa:".center(40))
-#print()
-#for linha_adv, linha_jog in zip(matriz_adversario, mapa_usuario):
-#print(" ".join(linha_adv).ljust(30), "   ", " ".join(linha_jog))
-
-def criar_mapa():
-    matriz_adversario = []
-    matriz_jogador = []
-
-    for i in range(12):
-        linha_adversario = []
-        linha_jogador = []
-        if i == 0 or i == 11:
-            linha_adversario = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
-            linha_jogador = [' '] + [letra for letra in ALFABETO[:10]] + [' ']
-        else:
-            linha_adversario = [' '] * 12
-            linha_jogador = [' '] * 12
-            if i >= 1 and i <= 9:
-                nmr = str(i)
-                lista = [' ', nmr]
-                linha_adversario[0] = str(i)
-                linha_jogador[0] = str(i)
-                linha_adversario[-1] = ''.join(lista)
-                linha_jogador[-1] = ''.join(lista)
-            elif i == 10:
-                linha_adversario[0] = str(i)
-                linha_jogador[0] = str(i)
-                linha_adversario[-1] = str(i)
-                linha_jogador[-1] = str(i)
-        matriz_adversario.append(linha_adversario)
-        matriz_jogador.append(linha_jogador)
-    
-
-
-    print("Mapa do adversário:".center(20), "Seu mapa:".center(40))
-    print( )
-    for linha_adv, linha_jog in zip(matriz_adversario, matriz_jogador):
-        print(" ".join(linha_adv).ljust(30), "", " ".join(linha_jog)) 
-
-    matrizes = [matriz_jogador, matriz_adversario]
-    return matrizes  
+countdown = [5, 4, 3, 2, 1]
+for i in countdown:
+    time.sleep(1)
+    print(i)
